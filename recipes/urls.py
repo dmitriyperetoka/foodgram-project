e28d40d4d9d92c2f8969091de1087691f05b8cbf
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-02-23 17:49:55 +0300

    Renamed profile page to author recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:author_username>', views.profile, name='profile'),
+    path('<str:author_username>', views.author_recipes, name='author_recipes'),
     path('<str:author_username>/<int:recipe_id>', views.recipe, name='recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,8 +5,8 @@ def index(request):
     return render(request, 'index.html')
 
 
-def profile(request, **kwargs):
-    return render(request, 'profile.html')
+def author_recipes(request, **kwargs):
+    return render(request, 'author_recipes.html')
 
 
 def recipe(request, **kwargs):
