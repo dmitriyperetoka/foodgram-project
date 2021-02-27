bb3683959c262e0851415822f9c1ae0f3bf417c2
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-02-27 17:53:49 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('favourites', views.FavouriteRecipes.as_view(), name='favourites'),
+    path('favourites', views.FavouriteRecipesView.as_view(), name='favourites'),
     path('subscriptions', views.subscriptions, name='subscriptions'),
 ]
