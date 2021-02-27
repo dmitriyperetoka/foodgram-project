33f032134af43c666e47908ad56351ad6b3bbfa7
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-02-27 14:37:00 +0300

    Put in order profile page

--- recipes/views.py
+++ recipes/views.py
@@ -28,10 +28,16 @@ class RecipeListView(ListView):
 class AuthorRecipeListView(ListView):
     template_name = 'profile.html'
     paginate_by = 3
+    author = None
 
     def get_queryset(self):
-        author = get_object_or_404(User, username=self.kwargs['username'])
-        return Recipe.objects.filter(author=author)
+        self.author = get_object_or_404(User, username=self.kwargs['username'])
+        return Recipe.objects.filter(author=self.author)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author'] = self.author
+        return context
 
 
 class RecipeDetailView(DetailView):
