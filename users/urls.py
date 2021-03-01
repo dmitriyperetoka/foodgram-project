d66bd5e44e95ec763e73702bd305a2b7cd8edda3
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-03-01 15:25:54 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
+    path('registration', views.RegistrationView.as_view(),
+         name='registration'),
     path('favourites', views.FavouriteRecipesView.as_view(),
          name='favourites'),
     path('subscriptions', views.SubscriptionsView.as_view(),
          name='subscriptions'),
-    path('registration', views.RegistrationView.as_view(),
-         name='registration'),
 ]
