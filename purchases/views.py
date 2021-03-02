e202f980ea2870412f65340ba4c810746dd6dbdc
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-03-02 15:15:30 +0300

    Update views.py

--- purchases/views.py
+++ purchases/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
 
 from .models import NewPurchaseList
 
 
+@method_decorator(login_required, 'dispatch')
 class NewPurchaseListView(TemplateView):
     template_name = 'purchase_list.html'
 
