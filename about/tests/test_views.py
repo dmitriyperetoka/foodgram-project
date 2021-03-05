c43248cca76755eced2f1530e26d393185c360e0
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-03-06 02:25:05 +0300

    Create test_views.py

---
+++ about/tests/test_views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import reverse
+
+from recipes.tests.base_classes import ViewsTestBase
+
+
+class AboutViewsTest(ViewsTestBase):
+    def test_template_used(self):
+        reverse_names_templates = [
+            (reverse('about:project'), 'about/project.html'),
+            (reverse('about:tech'), 'about/tech.html')
+        ]
+        self.check_template_used(reverse_names_templates)
