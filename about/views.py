ce79c1576b59a0decd948a0ee392a89b6b1ecfe2
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-03-21 15:39:22 +0300

    Put in order tech page

--- about/views.py
+++ about/views.py
@@ -7,3 +7,18 @@ class AboutProjectView(TemplateView):
 
 class AboutTechView(TemplateView):
     template_name = 'about/tech.html'
+    extra_context = {
+        'tech': [
+            ('Python', '3.9.2', 'https://www.python.org/'),
+            ('Django Framework', '3.1.6', 'https://www.djangoproject.com/'),
+            (
+                'Django REST Framework', '3.12.2',
+                'https://www.django-rest-framework.org/'
+            ),
+            ('Pillow', '8.1.1', 'https://python-pillow.org/'),
+            ('Gunicorn', None, 'https://gunicorn.org/'),
+            ('PostgreSQL', None, 'https://www.postgresql.org/'),
+            ('Nginx', None, 'https://nginx.org/'),
+            ('Docker', None, 'https://www.docker.com/'),
+        ]
+    }
