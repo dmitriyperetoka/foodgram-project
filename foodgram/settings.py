681c157bed1760b041f34304b9d58aba05db60ca
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-03-23 15:35:12 +0300

    Update settings.py

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -100,7 +100,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/foodgram/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL = '/media/'
