2cc0305ef215bc6fb1b39fe25ce0d5eaa73c3a9c
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-04-14 12:39:23 +0300

    Users test views

--- foodgram/users/tests/test_views.py
+++ foodgram/users/tests/test_views.py
@@ -1,16 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
 
-from foodgram.tests.base_classes import ViewsTestBase
+from foodgram import settings
+from foodgram.tests.base_classes import UsersViewsSetupBase, ViewsTestBase
 
 User = get_user_model()
 
 
-class UsersViewsTest(ViewsTestBase):
-    def setUp(self):
-        self.user = User.objects.create(username='someuser')
-        self.client.force_login(self.user)
-
+class UsersViewsTest(ViewsTestBase, UsersViewsSetupBase):
     def test_template_used(self):
         reverse_names_templates = [
             (reverse('users:favorite_list'), 'users/favorite_list.html'),
@@ -38,7 +35,6 @@ class UsersViewsTest(ViewsTestBase):
                 'registration/password_reset_done.html',
             ),
             (
-                # instead of reverse('password_reset_confirm')
                 '/personal/auth/reset/MQ/set-password/',
                 'registration/password_reset_confirm.html',
             ),
@@ -48,3 +44,27 @@ class UsersViewsTest(ViewsTestBase):
             ),
         ]
         self.check_template_used(reverse_names_templates)
+
+    def test_object_list_in_context(self):
+        favorites_queryset = self.user.favorites.all()
+        purchases_queryset = self.user.purchases.all()
+        subscriptions_queryset = self.user.subscriptions.all()
+
+        start_index = 0
+        stop_index = settings.PAGINATE_BY
+
+        pages = [
+            (
+                reverse('users:favorite_list'),
+                favorites_queryset, [start_index, stop_index],
+            ),
+            (
+                reverse('users:purchase_list'),
+                purchases_queryset, [None, None],
+            ),
+            (
+                reverse('users:subscription_list'),
+                subscriptions_queryset, [None, None],
+            ),
+        ]
+        self.check_object_list_in_context(pages)
